c0f016539379f346921026dbb337252f88176b90
loicpennequin <lo.pennequin@gmail.com>
2024-10-24 02:03:17 +0200

    wardrobe thingie

--- packages/client/src/pages/HomeView.vue
+++ packages/client/src/pages/HomeView.vue
@@ -5,13 +5,39 @@ import {
   type ParsedAsepriteSheet
 } from '@/utils/aseprite-parser';
 import { AnimatedSprite, Application, Assets, extensions } from 'pixi.js';
-import { onMounted, useTemplateRef } from 'vue';
+import { onMounted, ref, shallowRef, useTemplateRef, watchEffect } from 'vue';
 
 const canvas = useTemplateRef('pixiRoot');
 
+const helm = ref('tier1');
+const weapon = ref('tier1');
+const armor = ref('tier1');
+const vfx = ref('tier1');
+const app = shallowRef(new Application());
+
+const sprites = shallowRef<{
+  body?: AnimatedSprite;
+  weapon?: AnimatedSprite;
+  armor?: AnimatedSprite;
+  helm?: AnimatedSprite;
+  vfx?: AnimatedSprite;
+}>({});
+
+let sheets: ParsedAsepriteSheet;
+const updateTextures = () => {
+  if (!sprites.value.armor) {
+    return;
+  }
+
+  sprites.value.armor!.textures = sheets[armor.value].armor.animations.idle;
+  sprites.value.helm!.textures = sheets[helm.value].helm.animations.idle;
+  sprites.value.weapon!.textures = sheets[weapon.value].weapon.animations.idle;
+  sprites.value.vfx!.textures = sheets[vfx.value].vfx.animations.idle;
+};
+watchEffect(updateTextures);
+
 onMounted(async () => {
-  const app = new Application();
-  await app.init({
+  await app.value.init({
     background: '#222233',
     resizeTo: window,
     antialias: false,
@@ -31,23 +57,138 @@ onMounted(async () => {
   const { swordsmanSheets } = (await Assets.loadBundle('swordsman')) as {
     swordsmanSheets: ParsedAsepriteSheet;
   };
+  sheets = swordsmanSheets;
+  sprites.value = {
+    body: new AnimatedSprite(sheets.base.body.animations.idle),
+    armor: new AnimatedSprite(sheets[armor.value].armor.animations.idle),
+    helm: new AnimatedSprite(sheets[helm.value].helm.animations.idle),
+    weapon: new AnimatedSprite(sheets[weapon.value].weapon.animations.idle),
+    vfx: new AnimatedSprite(sheets[vfx.value].vfx.animations.idle)
+  };
 
-  const sprites = [
-    new AnimatedSprite(swordsmanSheets.base.body.animations.idle),
-    new AnimatedSprite(swordsmanSheets.tier3.armor.animations.idle),
-    new AnimatedSprite(swordsmanSheets.tier3.helm.animations.idle),
-    new AnimatedSprite(swordsmanSheets.tier3.weapon.animations.idle),
-    new AnimatedSprite(swordsmanSheets.tier3.vfx.animations.idle)
-  ];
-  sprites.forEach(sprite => {
-    app.stage.addChild(sprite);
+  [
+    sprites.value.body!,
+    sprites.value.armor!,
+    sprites.value.helm!,
+    sprites.value.weapon!,
+    sprites.value.vfx!
+  ].forEach(sprite => {
+    app.value.stage.addChild(sprite);
     sprite.anchor.set(0.5, 0.5);
-    sprite.position.set(app.screen.width / 2, app.screen.height / 2);
+    sprite.position.set(
+      app.value.screen.width / 2,
+      app.value.screen.height / 2
+    );
     sprite.scale.set(4);
   });
 });
 </script>
 
 <template>
-  <canvas ref="pixiRoot" />
+  <div class="grid">
+    <canvas ref="pixiRoot" />
+    <div class="ui">
+      <fieldset>
+        <legend>Armor</legend>
+        <label>
+          <input type="radio" v-model="armor" value="tier1" />
+          Tier 1
+        </label>
+        <label>
+          <input type="radio" v-model="armor" value="tier2" />
+          Tier 2
+        </label>
+        <label>
+          <input type="radio" v-model="armor" value="tier3" />
+          Tier 3
+        </label>
+        <label>
+          <input type="radio" v-model="armor" value="tier4" />
+          Tier 4
+        </label>
+      </fieldset>
+
+      <fieldset>
+        <legend>Helm</legend>
+        <label>
+          <input type="radio" v-model="helm" value="tier1" />
+          Tier 1
+        </label>
+        <label>
+          <input type="radio" v-model="helm" value="tier2" />
+          Tier 2
+        </label>
+        <label>
+          <input type="radio" v-model="helm" value="tier3" />
+          Tier 3
+        </label>
+        <label>
+          <input type="radio" v-model="helm" value="tier4" />
+          Tier 4
+        </label>
+      </fieldset>
+
+      <fieldset>
+        <legend>Weapon</legend>
+        <label>
+          <input type="radio" v-model="weapon" value="tier1" />
+          Tier 1
+        </label>
+        <label>
+          <input type="radio" v-model="weapon" value="tier2" />
+          Tier 2
+        </label>
+        <label>
+          <input type="radio" v-model="weapon" value="tier3" />
+          Tier 3
+        </label>
+        <label>
+          <input type="radio" v-model="weapon" value="tier4" />
+          Tier 4
+        </label>
+      </fieldset>
+
+      <fieldset>
+        <legend>VFX</legend>
+        <label>
+          <input type="radio" v-model="vfx" value="tier1" />
+          Tier 1
+        </label>
+        <label>
+          <input type="radio" v-model="vfx" value="tier2" />
+          Tier 2
+        </label>
+        <label>
+          <input type="radio" v-model="vfx" value="tier3" />
+          Tier 3
+        </label>
+        <label>
+          <input type="radio" v-model="vfx" value="tier4" />
+          Tier 4
+        </label>
+      </fieldset>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="postcss">
+.grid {
+  display: grid;
+  > * {
+    grid-column: 1;
+    grid-row: 1;
+  }
+}
+
+.ui {
+  pointer-events: none;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: flex-start;
+
+  fieldset {
+    pointer-events: auto;
+  }
+}
+</style>
