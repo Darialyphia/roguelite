4f64309ec1599629e39349172040e59322cc6bbf
loicpennequin <lo.pennequin@gmail.com>
2025-01-08 03:22:31 +0100

    turned off lights auto dimming as it seems to cause perf issues

--- packages/client/src/vfx/usePointLight.ts
+++ packages/client/src/vfx/usePointLight.ts
@@ -30,51 +30,51 @@ export const providePointLights = (camera: CameraContext) => {
   const MIN_ZOOM_LEVEL_FOR_ATTENUATION = 2;
 
   // Dims the lights depending on zoom level and proximity of other lights
-  onTick(() => {
-    const zoom = camera.viewport.value!.scaled;
-    const zoomDimmingFactor =
-      zoom < MIN_ZOOM_LEVEL_FOR_ATTENUATION
-        ? 0
-        : mapRange(1 - (config.MAX_ZOOM - zoom), [0, 1], [0, 0.15]);
+  // onTick(() => {
+  //   const zoom = camera.viewport.value!.scaled;
+  //   const zoomDimmingFactor =
+  //     zoom < MIN_ZOOM_LEVEL_FOR_ATTENUATION
+  //       ? 0
+  //       : mapRange(1 - (config.MAX_ZOOM - zoom), [0, 1], [0, 0.15]);
 
-    lights.forEach(light => {
-      light.root.alpha = 1 - zoomDimmingFactor;
-    });
-    for (let i = 0; i < lights.length; i++) {
-      for (let j = 0; j < lights.length; j++) {
-        const lightA = lights[i];
-        const lightB = lights[j];
+  //   lights.forEach(light => {
+  //     light.root.alpha = 1 - zoomDimmingFactor;
+  //   });
+  //   for (let i = 0; i < lights.length; i++) {
+  //     for (let j = 0; j < lights.length; j++) {
+  //       const lightA = lights[i];
+  //       const lightB = lights[j];
 
-        if (lightA === lightB) {
-          continue;
-        }
-        if (lightA.group === lightB.group) continue;
+  //       if (lightA === lightB) {
+  //         continue;
+  //       }
+  //       if (lightA.group === lightB.group) continue;
 
-        const distance = dist(
-          lightA.root.getGlobalPosition(),
-          lightB.root.getGlobalPosition()
-        );
+  //       const distance = dist(
+  //         lightA.root.getGlobalPosition(),
+  //         lightB.root.getGlobalPosition()
+  //       );
 
-        const dimmingFactor =
-          distance < PROXIMITY_THRESHOLD
-            ? (PROXIMITY_THRESHOLD - distance) / PROXIMITY_THRESHOLD
-            : 0;
+  //       const dimmingFactor =
+  //         distance < PROXIMITY_THRESHOLD
+  //           ? (PROXIMITY_THRESHOLD - distance) / PROXIMITY_THRESHOLD
+  //           : 0;
 
-        lightA.root.alpha = Math.max(
-          MIN_ALPHA,
-          lightA.root.alpha - dimmingFactor
-        );
-        lightB.root.alpha = Math.max(
-          MIN_ALPHA,
-          lightB.root.alpha - dimmingFactor
-        );
-        // lightA.root.alpha = Math.min(
-        //   MIN_ALPHA,
-        //   lightA.root.alpha - zoomDimmingFactor
-        // );
-      }
-    }
-  });
+  //       lightA.root.alpha = Math.max(
+  //         MIN_ALPHA,
+  //         lightA.root.alpha - dimmingFactor
+  //       );
+  //       lightB.root.alpha = Math.max(
+  //         MIN_ALPHA,
+  //         lightB.root.alpha - dimmingFactor
+  //       );
+  //       // lightA.root.alpha = Math.min(
+  //       //   MIN_ALPHA,
+  //       //   lightA.root.alpha - zoomDimmingFactor
+  //       // );
+  //     }
+  //   }
+  // });
   provide(POINT_LIGHTS_INJECTION_KEY, {
     registerLight(light) {
       lights.push(light);
